6a70b6af5abeac4b5e1e4a2de8d1ff39a575632c
Marc <marcpinson9@gmail.com>
2022-07-04 23:12:40 +0200

    Created base Car model & local DB

---
+++ TheCarHub/TheCarHub/Data/CarHubContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheCarHub.Models;
+
+namespace TheCarHub.Data
+{
+    public class CarHubContext : DbContext
+    {
+        public CarHubContext(DbContextOptions<CarHubContext> options): base(options)
+        {
+        }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
