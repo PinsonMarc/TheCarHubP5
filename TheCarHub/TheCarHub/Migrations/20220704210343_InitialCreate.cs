563cbfcb6b13939e65706aa95f1ca0a182cf54de
Marc <marcpinson9@gmail.com>
2022-07-04 23:12:40 +0200

    Created base Car model & local DB

---
+++ TheCarHub/TheCarHub/Migrations/20220704210343_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheCarHub.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    VIN = table.Column<string>(nullable: true),
+                    Year = table.Column<int>(nullable: false),
+                    Make = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    Trim = table.Column<string>(nullable: true),
+                    PurchaseDate = table.Column<DateTime>(nullable: false),
+                    PurchasePrice = table.Column<float>(nullable: false),
+                    Repairs = table.Column<string>(nullable: true),
+                    RepairCost = table.Column<string>(nullable: true),
+                    LotDate = table.Column<DateTime>(nullable: false),
+                    SaleDate = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
