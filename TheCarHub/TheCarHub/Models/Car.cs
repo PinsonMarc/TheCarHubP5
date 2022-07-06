c48e7eb099d0b9c09c375ca41374738da8babb3f
Marc <marcpinson9@gmail.com>
2022-07-06 19:42:12 +0200

    Improve car admin View

--- TheCarHub/TheCarHub/Models/Car.cs
+++ TheCarHub/TheCarHub/Models/Car.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -24,22 +25,27 @@ namespace TheCarHub.Models
 
         [StringLength(10)]
         public string Trim { get; set; }
-        
+
+        [DisplayName("Purchase Date")]
         [DataType(DataType.Date)]
         public DateTime PurchaseDate { get; set; }
 
+        [DisplayName("Purchase Price")]
         [Column(TypeName = "decimal(18, 2)")]
         [DataType(DataType.Currency)]
         public decimal PurchasePrice { get; set; }
         public string Repairs { get; set; }
 
+        [DisplayName("Repair Cost")]
         [Column(TypeName = "decimal(18, 2)")]
         [DataType(DataType.Currency)]
         public decimal RepairCost { get; set; }
 
+        [DisplayName("Lot Date")]
         [DataType(DataType.Date)]
         public DateTime LotDate { get; set; }
 
+        [DisplayName("Sale Date")]
         [DataType(DataType.Date)]
         public DateTime? SaleDate { get; set; }
         public Status Status { get; set; }
