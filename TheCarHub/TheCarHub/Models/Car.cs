d6e29d90842e4bbaf476a3c66812e31150fd49f6
Marc <marcpinson9@gmail.com>
2022-07-06 19:16:43 +0200

    Admin view scaffolding & improvement

--- TheCarHub/TheCarHub/Models/Car.cs
+++ TheCarHub/TheCarHub/Models/Car.cs
@@ -1,25 +1,54 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TheCarHub.Models
 {
     public class Car
     {
-        public string Id { get; set; }  
+        [BindNever]
+        public string Id { get; set; }
+
+        [StringLength(100)]
         public string VIN { get; set; }
+
+        [Range(1990, 2050)]
         public int Year { get; set; }
+
+        [StringLength(50)]
         public string Make { get; set; }
+
+        [StringLength(50)]
         public string Model { get; set; }
+
+        [StringLength(10)]
         public string Trim { get; set; }
+        
+        [DataType(DataType.Date)]
         public DateTime PurchaseDate { get; set; }
 
         [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
         public decimal PurchasePrice { get; set; }
         public string Repairs { get; set; }
 
         [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
         public decimal RepairCost { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime LotDate { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime? SaleDate { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public enum Status
+    {
+        Available,
+        NotAvailable,
+        Sold
     }
 }
